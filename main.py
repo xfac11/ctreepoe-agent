ddb29ff4bc477608f0f6b89722b64693528ebddb
xfac11 <xfac115@gmail.com>
2026-01-09 08:53:54 +0100

    Added argparse that parser the arguments and add helpful messages and error messages. Takes one argument from the command line and does type checking and can print a helpful message defined by me. Then the parsed argument is used by the model to create a response.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 def main():
     print("Hello from ctreepoe-agent!")
+    parser = argparse.ArgumentParser(description="ctreepoe-agent - A helping chat bot")
+    parser.add_argument("user_prompt", type=str, help="The message to the helpful chat bot")
+    args = parser.parse_args()
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
@@ -12,7 +16,7 @@ def main():
     if client == None:
         raise RuntimeError("Genai couldn't create a client")
 
-    response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=args.user_prompt)
     if response.usage_metadata == None:
         raise RuntimeError("usage_metadata was None, likely a failed API request")
     
