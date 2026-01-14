8db58d6768c6ca95bfb691c21355203c4fb5c8c8
xfac11 <xfac115@gmail.com>
2026-01-14 14:07:28 +0100

    Added a response loop to collect response candidates and function calls and adding them to the message listthat is given to the generate content function. The loop has a maximum of 20. More than that will return an error code of 1

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from google import genai
 from google.genai import types
 from prompts import system_prompt
 from call_functions import*
+def generate_content(client, model_name, contents, config):
+    return client.models.generate_content(
+        model=model_name,
+        contents=contents,
+        config=config
+        )
+    
 def main():
     load_dotenv()
     print("Hello from ctreepoe-agent!")
@@ -21,7 +28,6 @@ def main():
     )
     args = parser.parse_args()
     parts=[types.Part(text=args.user_prompt)]
-    messages = [types.Content(role="user", parts=parts)]
     model_name = 'gemini-2.5-flash'
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
@@ -30,42 +36,52 @@ def main():
     client = genai.Client(api_key=api_key)
     if client == None:
         raise RuntimeError("Genai couldn't create a client")
-
-    response = client.models.generate_content(
-        model=model_name,
-        contents=messages,
-        config=types.GenerateContentConfig(
+    config = types.GenerateContentConfig(
             system_instruction=system_prompt,
             temperature=0,
             tools=[available_functions],
-        ),
     )
-    if response.usage_metadata == None:
-        raise RuntimeError(
-            "usage_metadata was None, likely a failed API request"
-            )
-    
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
+
     function_results = []
-    if response.function_calls != None:
-        for call in response.function_calls:
-            function_call_result = call_function(call, True)
-            if len(function_call_result.parts) == 0:
-                raise Exception("Function call Parts list was empty")
-            function_response = function_call_result.parts[0].function_response
-            if function_response == None:
-                raise Exception("Parts[0] function response in function call result was None")
-            if function_response.response == None:
-                raise Exception("The function response response was None")
-            function_results.append(function_call_result.parts[0])
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
+    messages = [types.Content(role="user", parts=parts)]
+
+    for _ in range(20):
+        response = generate_content(client, model_name, messages, config)
+        if response.usage_metadata == None:
+            raise RuntimeError(
+                "usage_metadata was None, likely a failed API request"
+            )
+        candidates = response.candidates
+        if len(candidates) == 0:
+            raise Exception("Found no candidates (it should be)")
+        for candidate in candidates:
+            messages.append(candidate.content)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        if response.function_calls != None:
+            for call in response.function_calls:
+                function_call_result = call_function(call, True)
+                if len(function_call_result.parts) == 0:
+                    raise Exception("Function call Parts list was empty")
+                function_response = function_call_result.parts[0].function_response
+                if function_response == None:
+                    raise Exception("Parts[0] function response in function call result was None")
+                if function_response.response == None:
+                    raise Exception("The function response response was None")
+                function_results.append(function_call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+        else:
+            print(response.text)
+            return
+        
+        messages.append(types.Content(role="user", parts=function_results))
+    
+    print("Cannot solve the issue")
+    exit(1)
 
 
 if __name__ == "__main__":
